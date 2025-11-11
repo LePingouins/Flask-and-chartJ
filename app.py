a5e61f35293f1c7b01014bb0db45befeabc65f78
Olivier Goudreault <oligoudreault@gmail.com>
2025-11-11 17:25:58 -0500

    Modified app.py

--- app.py
+++ app.py
@@ -1,22 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
- return render_template('home.html')
+    return render_template('home.html')
 
-@app.route('/line-chart')
+@app.route('/line_chart')
 def line_chart():
- return render_template('line_chart.html')
+    return render_template('line_chart.html')
 
-@app.route('/bar-chart')
+@app.route('/bar_chart')
 def bar_chart():
- return render_template('bar_chart.html')
+    return render_template('bar_chart.html')
 
-@app.route('/pie-chart')
+@app.route('/pie_chart')
 def pie_chart():
- return render_template('pie_chart.html')
+    return render_template('pie_chart.html')
 
+# 👇 This part actually starts the Flask development server
 if __name__ == '__main__':
- app.run(debug=True)
+    app.run(debug=True)
+
+# from flask import Flask, render_template
+#
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def home():
+#  return render_template('home.html')
+#
+# @app.route('/line-chart')
+# def line_chart():
+#  return render_template('line_chart.html')
+#
+# @app.route('/bar-chart')
+# def bar_chart():
+#  return render_template('bar_chart.html')
+#
+# @app.route('/pie-chart')
+# def pie_chart():
+#  return render_template('pie_chart.html')
+#
+# if __name__ == '__main__':
+#  app.run(debug=True)
