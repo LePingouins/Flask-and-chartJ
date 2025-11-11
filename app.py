3ebf49dbd1de3fd41717257eadc36ff3822e4b60
Olivier Goudreault <oligoudreault@gmail.com>
2025-11-11 17:10:13 -0500

    added app

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+ return render_template('home.html')
 
+@app.route('/line-chart')
+def line_chart():
+ return render_template('line_chart.html')
+
+@app.route('/bar-chart')
+def bar_chart():
+ return render_template('bar_chart.html')
+
+@app.route('/pie-chart')
+def pie_chart():
+ return render_template('pie_chart.html')
 
 if __name__ == '__main__':
-    app.run()
+ app.run(debug=True)
